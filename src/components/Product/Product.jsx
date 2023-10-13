0236a926d634edad07114b06903a52f5be667e0e
asib <ahmedasib1@gmail.com>
2023-10-13 23:53:10 +0600

    cart style

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,23 @@
-import './Product.css'
+import './Product.css';
+import PropTypes from 'prop-types'
 
 const Product = (props) => {
-    const {name, price, shipping, stock, img, seller, quantity} = props.product;
+    const { name, price, shipping, stock, img, seller, quantity, ratings } = props.product;
     return (
         <div className="product">
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className="product-info">
+                <h6 className='product-name'>{name}</h6>
+                <p>price: ${price}</p>
+                <p>Manufacturer: {seller}</p>
+                <p>rating: {ratings} stars</p>
+            </div>
+            <button className='product-cart'>add to cart</button>
         </div>
     );
 };
 
+Product.propTypes = {
+    product: PropTypes.object, // Use the appropriate PropTypes type
+};
 export default Product;
