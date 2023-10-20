43cff492a780aa68b8c9910585ae59e6607cd111
asib <ahmedasib1@gmail.com>
2023-10-20 18:02:40 +0600

    review item style cmplete

--- src/components/OrderReview/OrderReview.jsx
+++ src/components/OrderReview/OrderReview.jsx
@@ -1,11 +1,20 @@
 import './OrderReview.css'
 import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
 
 const OrderReview = ({product}) => {
     const {name, id, quantity,img, price} = product;
     return (
         <div className='review'>
             <img src={img} alt="" />
+            <div className='order-info'>
+                <p className='title'>{name}</p>
+                <p>Price: <span className='orange-text'>${price}</span></p>
+                <p>Quantity: <span className='orange-text'>{quantity}</span></p>
+            </div>
+            <button className='delete-btn'><FontAwesomeIcon className='delete-icon' icon={faTrashCan} /></button>
         </div>
     );
 };
