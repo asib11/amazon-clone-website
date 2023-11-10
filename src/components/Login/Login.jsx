9df4bde0ce4998f2ec6de70df80a871d6f82a82a
asib ahmed <ahmedasib1@gmail.com>
2023-11-10 11:11:35 +0600

    sign up and login page build and design

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,8 +2,22 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div>
-            <h1>login ...</h1>
+        <div className='login-container'>
+            <h4>Login</h4>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="passowrd">Password</label>
+                    <input type="passowrd" name="passowrd" id="" required/>
+                </div>
+                <input type="submit" className='btn-submit' value="Login" />
+                <p>New to Amazon-Clone? <span>Create New Account</span></p>
+                <p>or</p>
+                <input type="submit" className='google-btn' value="Continue With Google" />
+            </form>
         </div>
     );
 };
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,8 +2,26 @@ import './SignUp.css'
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>signup coming soon...</h2>
+        <div className='login-container'>
+            <h4>Sign Up</h4>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="passowrd">Password</label>
+                    <input type="passowrd" name="passowrd" id="" required/>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="confirm">Confirm Password</label>
+                    <input type="passowrd" name="confirm" id="" required/>
+                </div>
+                <input type="submit" className='btn-submit' value="Sign Up" />
+                <p>Already have an account? <span>Login</span></p>
+                <p>or</p>
+                <input type="submit" className='google-btn' value="Continue With Google" />
+            </form>
         </div>
     );
 };
